dc968d3f654ff4498d5e8796cd9e15e58a057f43
haperezv <arcaico13@hotmail.com>
2022-11-03 15:18:35 -0500

    slack demo

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :rooms do
+    resources :messages
+  end
+  root 'pages#home'
+  devise_for :users
+  devise_scope :user do
+    get 'users', to: 'devise/sessions#new'
+  end
+  get 'user/:id', to: 'users#show', as: 'user'
+end
